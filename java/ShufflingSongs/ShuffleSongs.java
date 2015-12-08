eea346033d51451fdfb1e494841809a07b6ad608
MChen7209 <YJChen2922@outlook.com>
2015-12-08 01:53:58 -0600

    Modified Shuffling Songs
    -ShuffleSongs.java
       +Tested reversing a list using collect and recursion.
       +Modified the reading of the list a bit.
    
    -ShufflingSongs/compile.sh
       +Removed a redundant line.

--- java/ShufflingSongs/ShuffleSongs.java
+++ java/ShufflingSongs/ShuffleSongs.java
@@ -2,28 +2,40 @@ import java.util.*;
 import java.util.stream.*;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.function.IntPredicate;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.FileReader;
+import java.util.Scanner;
     
 class ShuffleSongs {
     public static void main(String[] args) {
-        // List<Integer> test = IntStream.rangeClosed(1,20)
-        //                                .boxed()
-        //                                  .collect(Collectors.toList());
-        ArrayList<String> songList = getSongList("songFile.txt");
-        Collections.shuffle(songList);
-        System.out.println(songList);
-        // Collections.shuffle(test);
-//         System.out.println(test);
+        Scanner input = new Scanner(System.in); 
+        do {
+            System.out.println("Enter file name: ");
+            String fileName = input.next();
+            try {
+                List<String> songList = getSongList(fileName);
+
+                //Inverting songs
+                System.out.println("Inverse Songs Using Collect:  " + invertSongsCollect(songList));
+                System.out.println("Inverse Songs using Recursion:" + invertSongsRecursive(songList));
+
+                //Using built in shuffle in collections.
+                Collections.shuffle(songList);
+                System.out.println("Shuffle Songs Using Built In: " + songList);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+
+            System.out.print("Again? (n to quit): ");
+        } while (!input.next().equals("n"));
     }
     
-    static ArrayList<String> getSongList(String fileName) {
+    static List<String> getSongList(String fileName) throws Exception {
         //Use file reader to get a list of songs copy and pasted in a notepad or something.
-        ArrayList<String> songList = new ArrayList<String>();
+        List<String> songList = new ArrayList<String>();
         String currentLine = null;
         try {
             BufferedReader reader = new BufferedReader(new FileReader(fileName));
@@ -31,8 +43,20 @@ class ShuffleSongs {
                 songList.add(currentLine);
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            throw new Exception("File Not Found");
         }
         return songList;
+    }
+
+    static List<String> invertSongsRecursive(List<String> songList) {
+        if(songList.size() <= 1)
+            return songList;
+        return Stream.concat(invertSongsRecursive(songList.subList(1, songList.size())).stream(), Arrays.asList(songList.get(0)).stream()).collect(Collectors.toList());
+    }
+
+    static List<String> invertSongsCollect(List<String> songList) {
+        //Last in first out using linked lists
+        return songList.stream()
+            .collect(LinkedList::new, LinkedList::addFirst, LinkedList::addAll);
     }
 }
--- java/ShufflingSongs/compile.sh
+++ java/ShufflingSongs/compile.sh
@@ -2,8 +2,6 @@
     mkdir classes
 
     export JUNIT_JAR="/opt/java/junit/junit-4.12.jar"
-
-    cd ./ShuffleSongs.java
     
     javac -Xlint:deprecation -Xlint:unchecked -d classes -classpath classes: *.java
     java -classpath classes: ShuffleSongs
